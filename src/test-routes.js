44536841f054933fd2f05c11877d47a470be5744
Joseph <z@charmed.tech>
2019-04-07 12:17:52 -0700

    Add routes to autopopulate roles and users

--- src/test-routes.js
+++ src/test-routes.js
@@ -2,16 +2,40 @@
 
 const router = require('express').Router();
 const auth = require('./auth/middleware.js');
-const Role = require('./auth/roles-model.js');
+const Roles = require('./auth/roles-model.js');
 const Users = require('./auth/users-model.js');
 
 router.get('/', (req, res, next) => {
   res.status(200).send('Server up and running...');
 });
 
+// autopopulate roles
+router.post('/autopopulate-roles', (req, res, next) => {
+  const admin = { role: 'admin', capabilities: ['create', 'read', 'update', 'delete'] };
+  const editor = { role: 'editor', capabilities: ['create', 'read', 'update'] };
+  const user = { role: 'user', capabilities: ['read'] };
+
+  [user, editor, admin].forEach(role => {
+    Roles.findOne({ role: role.role })
+      .then(result => {
+        if (!result) {
+          const r = new Roles(role);
+          r.save().then(role => {
+            console.log(`'${role.role}' successfully saved!`);
+          });
+        } else {
+          console.log(`Role '${role.role}' already exists...`);
+        }
+      })
+      .catch(console.error);
+  });
+
+  res.status(200).send('Populating roles...');
+});
+
 // Use to populate roles
 router.post('/roles', (req, res, next) => {
-  const role = new Role(req.body);
+  const role = new Roles(req.body);
   role
     .save()
     .then(role => {
@@ -20,7 +44,30 @@ router.post('/roles', (req, res, next) => {
     .catch(next);
 });
 
-// Use to see the virtual join
+// autopopulate users
+router.post('/autopopulate-users', (req, res, next) => {
+  const admin = { username: 'admin', password: 'admin', role: 'admin' };
+  const editor = { username: 'editor', password: 'editor', role: 'editor' };
+  const user = { username: 'user', password: 'user' };
+
+  [user, editor, admin].forEach(user => {
+    Users.findOne({ username: user.username })
+      .then(result => {
+        if (!result) {
+          const u = new Users(user);
+          u.save().then(user => {
+            console.log(`'${user.username}' successfully saved!`);
+          });
+        } else {
+          console.log(`User '${user.username}' already exists...`);
+        }
+      })
+      .catch(console.error);
+  });
+  res.status(200).send('Populating users...');
+});
+
+// Get all users
 router.get('/users', (req, res, next) => {
   Users.find().then(users => {
     users.forEach(user => {
