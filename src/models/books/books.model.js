457f291e425eac9be45fc4dd8767debc89387eed
Joseph <z@charmed.tech>
2019-04-07 20:26:55 -0700

    Update jsdoc

--- src/models/books/books.model.js
+++ src/models/books/books.model.js
@@ -3,6 +3,9 @@
 const ModelClass = require('../model.model.js');
 const BooksSchema = require('./books.schema.js');
 
+/**
+ * @param schema {object} Mongoose schema
+ */
 class BooksModel extends ModelClass {}
 
 module.exports = new BooksModel(BooksSchema);
